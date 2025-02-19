264fcce954aa502b37e57761fe0477d0789a3bc1
alexandre-kakal-akarah <alexandre.kakal@akarah.com>
2025-02-19 10:57:34 +0100

    feat: variable + providers

---
+++ providers.tf
@@ -0,0 +1,34 @@
+# définir des providers
+# doc azurerm providers https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+# définir des variables
+
+variable "subscription_id" {
+  description = "The Azure subscribtion ID"
+  type = string
+  sensitive = true
+}
